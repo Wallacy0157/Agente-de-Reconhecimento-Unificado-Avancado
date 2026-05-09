8d1ec624bcd9f8682d7e1eb713c154823cfafe80
Caio Vinícius Rodrigues Moreira <caiobiscas@gmail.com>
2026-05-09 00:10:05 -0300

    commit inicial: criacao backend

---
+++ api/src/main/java/com/ucb/agente_reconhecimento/AgenteReconhecimentoApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.agente_reconhecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenteReconhecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenteReconhecimentoApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ucb/agente_reconhecimento/AgenteReconhecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucb.agente_reconhecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenteReconhecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5f515d7b98eb8f4250a3a01b392d0d8119887039
Caio Vinícius Rodrigues Moreira <caiobiscas@gmail.com>
2026-05-23 04:35:39 -0300

    [111] - feat: Cria superclasse de auditoria

---
+++ api/src/main/java/com/ucb/agente_reconhecimento/domain/entities/EntidadeAuditavel.java
@@ -0,0 +1,28 @@
+package com.ucb.agente_reconhecimento.domain.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class EntidadeAuditavel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
+
+    @UpdateTimestamp
+    private LocalDateTime atualizadoEm;
+}
